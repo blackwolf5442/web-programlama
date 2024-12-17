1571134000e8000a76edeb3618c2b0f2180bc64e
aliosmanocalir <132233049+blackwolf5442@users.noreply.github.com>
2024-12-17 22:21:02 +0300

    Add files via upload

---
+++ Islem.cs
@@ -0,0 +1,12 @@
+﻿namespace proje.Models
+{
+    public class Islem
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public double Sure { get; set; } // Dakika cinsinden
+        public decimal Ucret { get; set; }
+        public int SalonId { get; set; }
+        public Salon Salon { get; set; }
+    }
+}
