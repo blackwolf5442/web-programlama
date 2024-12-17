4b8070e6de96b9b09e45302bd307554ed5169e03
aliosmanocalir <132233049+blackwolf5442@users.noreply.github.com>
2024-12-17 22:20:23 +0300

    Add files via upload

---
+++ Calisan.cs
@@ -0,0 +1,12 @@
+﻿namespace proje.Models
+{
+    public class Calisan
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public List<string> Uzmanliklar { get; set; } = new();
+        public string UygunlukSaatleri { get; set; } // Örneğin: 09:00-12:00, 14:00-18:00
+        public int SalonId { get; set; }
+        public Salon Salon { get; set; }
+    }
+}
