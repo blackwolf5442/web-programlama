453eeac3e1d8e3cf59dd113ed7a82b48f90cc525
aliosmanocalir <132233049+blackwolf5442@users.noreply.github.com>
2024-12-18 21:16:04 +0300

    Add files via upload

---
+++ KuaforDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace proje.Models
+{
+    public class KuaforDbContext:DbContext
+    {
+        public KuaforDbContext(DbContextOptions<KuaforDbContext> options) : base(options) { }
+        public DbSet<Salon> Salonlar { get; set; }
+        public DbSet<Islem> Islemler { get; set; }
+        public DbSet<Calisan> Calisanlar { get; set; }
+        public DbSet<Randevu> Randevular { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Randevu>()
+                .HasOne(r => r.Calisan)
+                .WithMany()
+                .HasForeignKey(r => r.CalisanId)
+                .OnDelete(DeleteBehavior.Cascade);  // Cascade delete devre dışı
+
+            modelBuilder.Entity<Randevu>()
+                .HasOne(r => r.Islem)
+                .WithMany()
+                .HasForeignKey(r => r.IslemId)
+                .OnDelete(DeleteBehavior.Restrict);  // Cascade delete devre dışı
+           
+        }
+        
+
+    }
+}
