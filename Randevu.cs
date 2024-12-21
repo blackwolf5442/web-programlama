d6893af6e5420d3854fadc677e4c5d873592e40a
aliosmanocalir <132233049+blackwolf5442@users.noreply.github.com>
2024-12-21 20:45:33 +0300

    Add files via upload

---
+++ Randevu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace proje.Models
+{
+    public class Randevu
+    {
+        [Key]
+        public int Id { get; set; }  // Birincil anahtar
+
+        [Required]
+        public int CalisanId { get; set; }  // Yabancı Anahtar - Çalışan
+
+        [ForeignKey("CalisanId")]
+        public Calisan Calisan { get; set; }  // Navigation Property
+
+        [Required]
+        public int IslemId { get; set; }  // Yabancı Anahtar - İşlem
+
+        [ForeignKey("IslemId")]
+        public Islem Islem { get; set; }  // Navigation Property
+
+        [Required]
+        public DateTime TarihSaat { get; set; }  // Randevu Tarihi ve Saati
+
+        public bool Onaylandi { get; set; } = false;  // Onay durumu
+    }
+
+}
+
