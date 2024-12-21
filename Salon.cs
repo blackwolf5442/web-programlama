8dc2bf073ba0fba6f6cb7fb53fb8572216f10fce
aliosmanocalir <132233049+blackwolf5442@users.noreply.github.com>
2024-12-21 20:45:45 +0300

    Add files via upload

---
+++ Salon.cs
@@ -0,0 +1,16 @@
+﻿namespace proje.Models
+{
+    public class Salon
+    {
+       
+            public int Id { get; set; }
+            public string Ad { get; set; }
+            public string CalismaSaatleri { get; set; } // Örneğin: 09:00-18:00
+            public List<Islem> Islemler { get; set; } = new();
+            public List<Calisan> Calisanlar { get; set; } = new();
+            
+            //public ICollection<Calisan> Calisanlar { get; set; }
+
+
+    }
+}
