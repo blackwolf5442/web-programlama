01db424a659e9b59b7b7bca26b14e75b2d37fbf9
aliosmanocalir <132233049+blackwolf5442@users.noreply.github.com>
2024-12-27 14:08:00 +0300

    Update KuaforDbContext.cs

--- KuaforDbContext.cs
+++ KuaforDbContext.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
 namespace proje.Models
@@ -10,14 +10,28 @@ namespace proje.Models
         public DbSet<Islem> Islemler { get; set; }
         public DbSet<Calisan> Calisanlar { get; set; }
         public DbSet<Randevu> Randevular { get; set; }
+        public DbSet<UyeOl> Üyeler { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
+            // Randevu için yapılandırma
+            modelBuilder.Entity<Randevu>(entity =>
+            {
+                entity.Property(r => r.AdminOnayli)
+                      .IsRequired()
+                      .HasMaxLength(50)
+                      .HasDefaultValue("Beklemede"); // Varsayılan değer
+            });
+
             modelBuilder.Entity<Randevu>()
                 .HasOne(r => r.Calisan)
                 .WithMany()
                 .HasForeignKey(r => r.CalisanId)
                 .OnDelete(DeleteBehavior.Cascade);  // Cascade delete devre dışı
+
+            
 
             modelBuilder.Entity<Randevu>()
                 .HasOne(r => r.Islem)
